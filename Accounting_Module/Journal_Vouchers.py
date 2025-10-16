d892fde25e5171afb60ea7188595a10176cc4351
Sagan Krishna Tamrakar <87718989+Unknown3369@users.noreply.github.com>
2025-10-16 09:52:37 +0545

    updates

--- Accounting_Module/Journal_Vouchers.py
+++ Accounting_Module/Journal_Vouchers.py
@@ -13,9 +13,25 @@ class MainPage:
         self.driver = driver
         self.wait = WebDriverWait(self.driver, 15)
 
+        self.accounting_module = (By.XPATH, "//span[normalize-space(text())='Accounting Module']")
+        self.transactions = (By.XPATH, "//span[normalize-space(text())='Transactions']")
+        self.chart_of_accounts = (By.LINK_TEXT, "Voucher Entries")
+        self.journal_voucher = (By.LINK_TEXT, "Journal Voucher")
+        self.refno = (By.XPATH, "//input[@id='refno']")
+        self.remarks = (By.XPATH, "//input[@name='REMARKS']")
+        self.select_account = (By.XPATH, "//input[@id='ACCODEInput_0']")
+        self.selected_account = (By.XPATH, "//div[@title='TheTestCustom']")
+        self.amount_dr = (By.XPATH, "//input[@id='DrAmtInput_0']")
+        self.narration = (By.XPATH, "//input[@id='narration_0']")
+        self.save_button = (By.XPATH, "//button[contains(text(),'F6 SAVE')]")
+        self.select_account_n = (By.XPATH, "//input[@id='ACCODEInput_1']")
+        self.selected_account_n = (By.XPATH, "//div[@title='Test Account']")
+        self.cr_amount = (By.XPATH, "//input[@id='CrAmtInput_1']")
+        self.no_button = (By.XPATH, "//button[contains(text(),'No')]")
+
     def open_accounting_module(self):
         account = self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//span[normalize-space(text())='Accounting Module']"))
+            EC.element_to_be_clickable(self.accounting_module)
         )
         account.click()
         print("Navigated to Accounting Module.")
@@ -25,21 +41,20 @@ class MainPage:
         print("Switched to Accounting Module tab.")
     
     def open_journal_voucher(self):
-        masters = self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//span[normalize-space(text())='Transactions']"))
+        transactions = self.wait.until(
+            EC.element_to_be_clickable(self.transactions)
         )
-        ActionChains(self.driver).move_to_element(masters).click().perform()
+        ActionChains(self.driver).move_to_element(transactions).click().perform()
         print("Hovered and clicked on Transactions menu.")
 
         chart_of_accounts = self.wait.until(
-            EC.presence_of_element_located((By.LINK_TEXT, "Voucher Entries"))
+            EC.presence_of_element_located(self.chart_of_accounts)
         )
         ActionChains(self.driver).move_to_element(chart_of_accounts).perform()
         print("Hovered over Voucher Entries.")
 
         ledger_group = self.wait.until(
-            EC.element_to_be_clickable(
-                (By.LINK_TEXT, "Journal Voucher"))
+            EC.element_to_be_clickable(self.journal_voucher)
         )
         self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", ledger_group)
         time.sleep(2)  
@@ -49,69 +64,69 @@ class MainPage:
 
     def add_voucher(self, reference_number: str, enter_remarks: str, debit_amount: int, credit_amount: int):
         refno = self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//input[@id='refno']"))
+            EC.element_to_be_clickable(self.refno)
         )
         refno.clear()
         refno.send_keys(reference_number)
         print(f"Entered Reference Number: {reference_number}")
 
         remarks = self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//input[@name='REMARKS']"))
+            EC.element_to_be_clickable(self.remarks)
         )
         remarks.clear()
         remarks.send_keys(enter_remarks)
         print(f"Entered Remarks: {enter_remarks}")
 
         select_account = self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//input[@id='ACCODEInput_0']"))
+            EC.element_to_be_clickable(self.select_account)
         )
         select_account.send_keys(Keys.ENTER)
 
         selected_account = self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//div[@title='TheTestCustom']"))
+            EC.element_to_be_clickable(self.selected_account)
         )
         selected_account_click = ActionChains(self.driver).double_click(selected_account)
         selected_account_click.perform()
 
         dr_amount = self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//input[@id='DrAmtInput_0']"))
+            EC.element_to_be_clickable(self.amount_dr)
         )
         dr_amount.clear()
         dr_amount.send_keys(debit_amount)
         print(f"Entered Debit Amount: {debit_amount}")
 
         narration = self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//input[@id='narration_0']"))
+            EC.element_to_be_clickable(self.narration)
         )
         narration.send_keys(Keys.ENTER)
         print("Entered Next Line")
 
         select_account_n = self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//input[@id='ACCODEInput_1']"))
+            EC.element_to_be_clickable(self.select_account_n)
         )
         select_account_n.send_keys(Keys.ENTER)
 
         # Always locate the element right before performing the action
         ActionChains(self.driver).double_click(
-            self.wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@title='TheTestCustom']")))
+            self.wait.until(EC.element_to_be_clickable( self.selected_account_n))
         ).perform()
 
 
         cr_amount = self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//input[@id='CrAmtInput_1']"))
+            EC.element_to_be_clickable(self.cr_amount)
         )
         cr_amount.clear()
         cr_amount.send_keys(credit_amount)
         print(f"Entered Debit Amount: {credit_amount}")
 
         save_button = self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'F6 SAVE')]"))
+            EC.element_to_be_clickable(self.save_button)
         )
         save_button.click()
         print("Clicked on Save button.")
 
         no_btn = self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//button[@id='nobtn' and contains(@class, 'btn-warning') and normalize-space(text())='No']"))
+            EC.element_to_be_clickable(self.no_button)
         )
         no_btn.click()
         print("Clicked 'No' on the confirmation dialog.")
---
+++ Accounting_Module/party_opening_bl.py
@@ -0,0 +1,81 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from login_details import login_to_ims
+
+class MainPage:
+   def __init__(self, driver):
+      self.driver = driver
+      self.wait = WebDriverWait(self.driver, 15)
+
+      self.accounting_module = (By.XPATH, "//span[normalize-space(text())='Accounting Module']")
+      self.transactions = (By.XPATH, "//span[normalize-space(text())='Transactions']")
+      self.opening_entries = (By.LINK_TEXT, "Opening Entries")
+      self.party_opening_bl = (By.LINK_TEXT, "Party Opening B/L")
+      self.refno = (By.XPATH, "//input[@id='refno']")
+      self.select_account = (By.XPATH, "//input[@id='ACCODEInput_0']")
+      self.account_select = (By.XPATH, "//div[@title='Milk Chocolate Vendor']")
+      self.amount_dr = (By.XPATH, "//input[@id='AMOUNTDR_0']")
+      self.narration = (By.XPATH, "//input[@id='NARRATION']")
+      self.save_button = (By.XPATH, "//button[@id='btnSave']")
+      
+   def open_accounting_module(self):
+      account = self.wait.until(
+            EC.element_to_be_clickable(self.accounting_module)
+      )
+      account.click()
+      print("Navigated to Accounting Module.")
+
+      WebDriverWait(self.driver, 10).until(lambda d: len(d.window_handles) > 1)
+      self.driver.switch_to.window(self.driver.window_handles[-1])
+      print("Switched to Accounting Module tab.")
+
+   def open_party_opening_bl(self):
+      transactions = self.wait.until(
+         EC.element_to_be_clickable(self.transactions)
+      )
+      ActionChains(self.driver).move_to_element(transactions).click().perform()
+      print("Hovered and Clicked on Transactions menu.")
+      opening_entries = self.wait.until(
+         EC.presence_of_element_located(self.opening_entries)
+      )
+      opening_entries.click()
+      print("Clicked on opening entries.")
+      ledger_opening_bl = self.wait.until(
+         EC.presence_of_element_located(self.party_opening_bl)
+      )
+      ledger_opening_bl.click()
+      print("Clicked on Party Opening B/L.")
+
+   def add_party_opening_details(self, reference_number: str):
+      refno = self.wait.until(
+         EC.element_to_be_clickable(self.refno)
+      )
+      refno.clear()
+      refno.send_keys(reference_number)
+      print(f"Entered Reference Number: {reference_number}")
+
+      select_acc = self.wait.until(
+         EC.element_to_be_clickable(self.select_account)
+      )
+      select_acc.send_keys(Keys.ENTER)
+
+      account_select = self.wait.until(
+         EC.element_to_be_clickable(self.account_select)
+      )
+      actions_select = ActionChains(self.driver).double_click(account_select).perform()
+      print("Selected Account")
+
+
+
+
+if __name__ == "__main__":
+   driver = login_to_ims()
+   main_page = MainPage(driver)
+   main_page.open_accounting_module()
+   main_page.open_party_opening_bl()
+   # Add further interactions as needed
+   driver.quit()
