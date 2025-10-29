927b90cb6782571e29abc31f8b6556af231176e0
Sagan Krishna Tamrakar <87718989+Unknown3369@users.noreply.github.com>
2025-10-29 12:19:36 +0545

    added random anme and hscode generation for add product

--- PYTEST/Tests/test_add_prod.py
+++ PYTEST/Tests/test_add_prod.py
@@ -2,6 +2,7 @@ from PYTEST.pages.Login import login
 from PYTEST.pages.Add_product import Add_prod
 from selenium import webdriver
 import pytest
+import random
 
 
 def test_add_prod(driver: webdriver):
@@ -9,7 +10,18 @@ def test_add_prod(driver: webdriver):
    add_prod_page = Add_prod(driver)
    login_page.perform_login("Testuser", "Test@1234")
    add_prod_page.masters_click_test(driver)
-   add_prod_page.add_prod_test(driver, "Testitemname", "Test1121", "Testdescription", 100)
+
+   def generate_random_string(length):
+      letters = "abcdefghijklmnopqrstuvwxyz"
+      return "".join(random.choice(letters) for _ in range(length))
+   random_string = generate_random_string(10)
+
+   def generate_random_hscode(length):
+      random_hs = random.randint(1000, 9999)
+      return str(random_hs)
+      
+   
+   add_prod_page.add_prod_test(driver, random_string, generate_random_hscode(4), "Testdescription", 100)
 
 if __name__ == "__main__":
    pytest.main()
