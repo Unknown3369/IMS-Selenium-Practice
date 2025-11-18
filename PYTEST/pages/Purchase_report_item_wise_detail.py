eba7cd2833e06510e16aa9ae8e54b676c674488b
Sagan Krishna Tamrakar <87718989+Unknown3369@users.noreply.github.com>
2025-11-18 16:53:12 +0545

    test is still not comleted issue with dropdown selection

--- PYTEST/pages/Purchase_report_item_wise_detail.py
+++ PYTEST/pages/Purchase_report_item_wise_detail.py
@@ -15,10 +15,11 @@ class Purchase_report_item_wise_detail:
 
       self.reports = (By.XPATH, "//span[contains(text(),'Reports')]")
       self.purchase_report = (By.LINK_TEXT, "Purchase Reports")
-      self.purchase_report_item_wise = (By.XPATH, "//span[normalize-space()='Purchase Report - Item Wise']")
-      self.branch_dropdown = (By.XPATH, "//select[@class='form-control input-text ng-untouched ng-pristine ng-valid']")
-      self.item_select = (By.XPATH, "//input[@placeholder='Press Enter to select Items']")
-      self.select_item_code = (By.XPATH, "//select[@style='width: 100%' and contains(@class,'ng-valid')]")
+      self.purchase_report_item_wise = (By.XPATH, "//span[normalize-space()='Purchase Report - Item Wise Detail']")
+      self.branch_dropdown = (By.XPATH, "//select[option[@disabled and normalize-space()='Select Branch']]")
+      self.item_name = (By.XPATH, "//input[@placeholder='Press Enter to select Items']")
+      self.item_select = (By.XPATH, "//select[option[text()='Select Filter Option']]")
+      self.select_item_code = (By.XPATH, "//select[option[text()='ITEM CODE'] and option[text()='DESCRIPTION']]")
       self.search_item = (By.XPATH, "//input[@placeholder='Enter keyword to search']")
       self.select_search_item = (By.XPATH,"//input[@type='checkbox']")
       self.ok_button = (By.XPATH, "//button[normalize-space()='OK']")
@@ -51,42 +52,56 @@ class Purchase_report_item_wise_detail:
       time.sleep(1)
       self.driver.execute_script("arguments[0].click();", purchase_report_item_wise)
       print("Purchase Report - Item Wise clicked successfully!")
+      time.sleep(3)
 
-   def open_item_wise_detail_report(self, enter_item_code: str):
+   def open_item_wise_detail_report(self):
       # Branch Selection
       branch_dropdown = self.wait.until(
          EC.presence_of_element_located(self.branch_dropdown)
          )
+      time.sleep(2)
       select_branch = Select(branch_dropdown)
       select_branch.select_by_visible_text("ALL")
       print("Branch selected successfully!")
 
+      item_name = self.wait.until(
+         EC.presence_of_element_located(self.item_name)
+         )
+      item_name.click()
+      item_name.send_keys(Keys.ENTER)
+      time.sleep(2)
+      print("Item Name selected successfully!")
+
       item_select = self.wait.until(
          EC.presence_of_element_located(self.item_select)
          )
-      item_select.send_keys(Keys.ENTER)
+      self.driver.execute_script("arguments[0].click();", item_select)
       select_item_code = self.wait.until(
          EC.presence_of_element_located(self.select_item_code)
          )
-      select_item_code.click()
-      select_item_code.select_by_visible_text("ITEM CODE")
+      select_item_code_dropdown = Select(select_item_code)
+      time.sleep(2)
+      select_item_code_dropdown.select_by_visible_text("ITEM CODE")
       print("Item Code selected successfully!")
 
+   def fill_item_wise_detail_report(self, enter_item_code: str):
       search_item = self.wait.until(
          EC.presence_of_element_located(self.search_item)
          )
+      search_item.click()
       search_item.send_keys(enter_item_code)
       select_search_item = self.wait.until(
          EC.presence_of_element_located(self.select_search_item)
          )
       select_search_item.click()
       print(f"Item code {enter_item_code} entered successfully!")
-
+      
+   def run_item_wise_detail_report(self):
       ok_button = self.wait.until(
          EC.presence_of_element_located(self.ok_button)
          )
-      
-   def run_item_wise_detail_report(self):
+      ok_button.click()
+      print("OK button clicked successfully!")
       # Run report
       run_button = self.wait.until(
          EC.presence_of_element_located(self.run_button)
@@ -95,3 +110,4 @@ class Purchase_report_item_wise_detail:
       time.sleep(1)
       self.driver.execute_script("arguments[0].click();", run_button)
       print("Run button clicked successfully!")
+      
