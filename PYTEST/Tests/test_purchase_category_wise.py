0bd4522272723b01c2241495a58d9972b2b663f5
Sagan Krishna Tamrakar <87718989+Unknown3369@users.noreply.github.com>
2025-12-04 11:20:26 +0545

    --

--- PYTEST/Tests/test_purchase_category_wise.py
+++ PYTEST/Tests/test_purchase_category_wise.py
@@ -19,3 +19,4 @@ def test_purchase_category_wise(driver):
       screenshot_path = f"screenshots/sales_report_{int(time.time())}.png"
       driver.save_screenshot(screenshot_path)
       allure.attach.file(screenshot_path, name="Purchase Report Generated", attachment_type=allure.attachment_type.PNG)
+
